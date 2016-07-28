31854bf491ecb8d4228c9d06a97ddfc5b104da25
saki <manicon22@gmail.com>
2016-07-28 10:30:43 +0000

    修正

--- app/controllers/ranking_controller.rb
+++ app/controllers/ranking_controller.rb
@@ -1,13 +1,17 @@
 class RankingController < ApplicationController
     def have
-        @haves = Have.limit(10).group('item_id').order('count_item_id desc').count('item_id').keys
-        @items = Item.find(@haves)
-        @item
+        # @haves = Have.limit(10).group('item_id').order('count_item_id desc').count('item_id').keys
+        # @items = Item.find(@haves)
+        # @item
+        item_counts = Have.group(:item_id).order('count_id desc').limit(10).count(:id)
+        @ranking = item_counts.transform_keys!{ |key| Item.find(key) }
     end
     
     def want
-        @wants = Want.limit(10).group('item_id').order('count_item_id desc').count('item_id').keys
-        @items = Item.find(@wants)
-        @item
+        # @wants = Want.limit(10).group('item_id').order('count_item_id desc').count('item_id').keys
+        # @items = Item.find(@wants)
+        # @item
+        item_counts = Want.group(:item_id).order('count_id desc').limit(10).count(:id)
+        @ranking = item_counts.transform_keys!{ |key| Item.find(key) }
     end
 end
